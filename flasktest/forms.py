4c95d205b252c57e6fd155a3980d76ca29007cc3
Simone Giuri <s.giuri@issm.it>
2022-11-01 16:48:24 +0100

    login and filepicture (wp)

--- flasktest/forms.py
+++ flasktest/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+from flask_login import current_user
+from flasktest.models import User, Post
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -12,6 +16,39 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                 validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("The Username is not available!")
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError("The email is not available!")
+
+
+class UpdateUserAccount(FlaskForm):
+    username = StringField('Username',
+                                validators= [DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                                validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    def validate_username(self, username):
+        if current_user.username != username.data:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("The Username is not available!")
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError("The email is not available!")
+
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
@@ -25,5 +62,6 @@ class ResumePasswordForm(FlaskForm):
     email = StringField('Email',
                                 validators=[DataRequired(), Email()])
     submit = SubmitField('Reset')
+
 
 
--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from flasktest import app
 
+"""Per inizializzare il database
+from flasktest import app
+from flasktest import db
+from flasktest.models import User, Post
+with app.app_context():
+    db.create_all()
+"""
+
 
 if __name__=="__main__":
     app.run(debug=True)
